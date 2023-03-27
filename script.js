186879a8ae45eed3c0bdb9a3c499b50217ccae79
Hardik Choudhary <hardik109bkp@gmail.com>
2023-03-27 19:37:43 +0530

    made slider responsive

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const arrows =document.querySelectorAll(".slider");
 const movieLists = document.querySelectorAll(".movie-list");
-
 arrows.forEach((arrow,i)=>{
     let btnCounter=0;
     arrow.addEventListener("click",(e)=>{
         btnCounter++;
         let movieList=movieLists[i];
-        if(btnCounter<=2){
+        let moviListItems = movieList.children;
+        let slider = movieLists[i].nextElementSibling;
+        let locationFinal=slider.getBoundingClientRect();
+        let locationTarget= moviListItems[5].getBoundingClientRect();
+        // btnCounter<=2
+        if(locationTarget.left>locationFinal.left){
         movieList.style.transform=`translateX(${movieList.computedStyleMap().get("transform")[0].x.value-550}px)`;
         }
         else{
